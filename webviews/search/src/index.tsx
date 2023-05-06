d12635aa7c69a99d69b3b58154fc79f9436922c1
Matthias Behr <mbehr@mcbehr.de>
2023-05-06 15:48:12 +0200

    chore(webview): update to latest versions

--- webviews/search/src/index.tsx
+++ webviews/search/src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-ReactDOM.render(
-    <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+    </StrictMode>);
