505590a4e9ed3f31f13fc8168c16028152340b4f
Matthias Behr <mbehr@mcbehr.de>
2024-03-09 20:39:11 +0100

    feat(search): add support for ecu/apid/ctid in search panel
    Searches for ECU/APID/CTID can be done by starting with
    @ECU:APID:CTID search text ...
    
    See [Docs/searchPanel](https://mbehr1.github.io/dlt-logs/docs/searchPanel) for details and examples.

--- docs/dlt-logs/sidebars.js
+++ docs/dlt-logs/sidebars.js
@@ -1,23 +1,33 @@
 module.exports = {
   dltLogsSideBar: {
-    'DLT-Logs':
-      [
-        {
-          "type": "category",
-          "label": "Guides",
-          "items": ['installFirstUse', 'genericSettings', 'filterReference', 'configsReference'],
-        },
-        {
-          "type": "category",
-          "label": "Features",
-          "items": ['lifecycleDetection', 'toggleSortOrder', 'timeSync', 'reports', 'exportAndFilter', 'fileTransfer', 'someIpPlugin', 'canPlugin', 'nonVerbosePlugin', 'rewritePlugin'],
-          "collapsed": false
-        },
-        {
-          "type": "link",
-          "label": "Changelog",
-          href: "https://github.com/mbehr1/dlt-logs/blob/master/CHANGELOG.md"
-        }
-      ]
-  }
-};
+    'DLT-Logs': [
+      {
+        type: 'category',
+        label: 'Guides',
+        items: ['installFirstUse', 'genericSettings', 'filterReference', 'configsReference'],
+      },
+      {
+        type: 'category',
+        label: 'Features',
+        items: [
+          'lifecycleDetection',
+          'toggleSortOrder',
+          'searchPanel',
+          'reports',
+          'exportAndFilter',
+          'fileTransfer',
+          'someIpPlugin',
+          'canPlugin',
+          'nonVerbosePlugin',
+          'rewritePlugin',
+        ],
+        collapsed: false,
+      },
+      {
+        type: 'link',
+        label: 'Changelog',
+        href: 'https://github.com/mbehr1/dlt-logs/blob/master/CHANGELOG.md',
+      },
+    ],
+  },
+}
